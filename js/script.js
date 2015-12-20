d114d61dcfdfd6fe382d33584d33e45ea6b479f5
Nazar <nazik542@ukr.net>
2015-12-20 21:30:43 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){ 
-	
+	var window_height = $(window).height();
+	console.log(window_height)
+	var window_width = $(window).width();
+	var header_height = parseInt($(".main_header").css("height"));
+		if(window_height<window_width)
+			$(".main_section").css("height",window_height - header_height+"px");
+		else{
+			$(".main_section").css("height",window_width *0.5 +"px");
+		}
+
+	$(window).resize(function(){
+		window_height = $(window).height();
+		window_width = $(window).width();
+		header_height = parseInt($(".main_header").css("height"));
+		if(window_height<window_width)
+			$(".main_section").css("height",window_height - header_height+"px");
+		else{
+			$(".main_section").css("height",window_width *0.5 +"px");
+		}
+	})
 }); 
